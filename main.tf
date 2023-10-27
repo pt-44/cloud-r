4c839150741d98832c465eb1138ba59f27b8839e
pt-44 <skizzik44@gmail.com>
2023-10-26 22:25:56 -0700

    add cloudfront config to main.tf

--- main.tf
+++ main.tf
@@ -85,3 +85,60 @@ output "website_url" {
 }
 
 data "aws_region" "current" {}
+
+# AWS CloudFront Distribution
+resource "aws_cloudfront_distribution" "cloudresume_distribution" {
+  origin {
+    domain_name = "cloudresume-pt2.s3.amazonaws.com"
+    origin_id   = "cloudresumeS3Origin"
+  }
+
+  price_class = "PriceClass_100" # North America and Europe
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CloudFront Distribution for cloudresume-pt2"
+  default_root_object = "index.html"
+
+  aliases = ["resume.3lack.co"] # Alternate domain names
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "cloudresumeS3Origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = "arn:aws:acm:us-east-1:796608557876:certificate/7f0b76df-bbd0-4fa7-ae9e-b773fd702f54"
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name    = "cloudresume-pt2-distribution"
+    Purpose = "CloudFront Distribution for Public Resume Storage"
+  }
+}
+
+# Output the CloudFront distribution's domain name (URL)
+output "cloudfront_distribution_url" {
+  value = aws_cloudfront_distribution.cloudresume_distribution.domain_name
+}
